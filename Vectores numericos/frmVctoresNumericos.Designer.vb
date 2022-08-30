306cbbded64f6a63124d9258dbe7a89ee3e30add
RodrigoDev <r1989castellano@gmail.com>
2022-08-30 20:21:43 -0300

    Agregar archivos de proyecto.

---
+++ Vectores numericos/frmVctoresNumericos.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmVctoresNumericos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.txtImporte = New System.Windows.Forms.TextBox()
+        Me.cmbMes = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dgvVentas = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnListar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvVentas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.cmbMes)
+        Me.GroupBox1.Controls.Add(Me.txtImporte)
+        Me.GroupBox1.Controls.Add(Me.btnCargar)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(252, 131)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Carga de datos"
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Location = New System.Drawing.Point(107, 96)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCargar.TabIndex = 1
+        Me.btnCargar.Text = "Cargar"
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'txtImporte
+        '
+        Me.txtImporte.Location = New System.Drawing.Point(83, 70)
+        Me.txtImporte.Name = "txtImporte"
+        Me.txtImporte.Size = New System.Drawing.Size(121, 20)
+        Me.txtImporte.TabIndex = 2
+        '
+        'cmbMes
+        '
+        Me.cmbMes.FormattingEnabled = True
+        Me.cmbMes.Location = New System.Drawing.Point(83, 43)
+        Me.cmbMes.Name = "cmbMes"
+        Me.cmbMes.Size = New System.Drawing.Size(121, 21)
+        Me.cmbMes.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Mes :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Importe :"
+        '
+        'dgvVentas
+        '
+        Me.dgvVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvVentas.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
+        Me.dgvVentas.Location = New System.Drawing.Point(13, 149)
+        Me.dgvVentas.Name = "dgvVentas"
+        Me.dgvVentas.Size = New System.Drawing.Size(240, 183)
+        Me.dgvVentas.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Mes"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Ventas"
+        Me.Column2.Name = "Column2"
+        '
+        'btnListar
+        '
+        Me.btnListar.Location = New System.Drawing.Point(168, 338)
+        Me.btnListar.Name = "btnListar"
+        Me.btnListar.Size = New System.Drawing.Size(75, 23)
+        Me.btnListar.TabIndex = 6
+        Me.btnListar.Text = "Listar"
+        Me.btnListar.UseVisualStyleBackColor = True
+        '
+        'frmVctoresNumericos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 382)
+        Me.Controls.Add(Me.btnListar)
+        Me.Controls.Add(Me.dgvVentas)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmVctoresNumericos"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ventas del año"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.dgvVentas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmbMes As ComboBox
+    Friend WithEvents txtImporte As TextBox
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents dgvVentas As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents btnListar As Button
+End Class
---
+++ Vectores numericos/frmVctoresNumericos.vb
@@ -0,0 +1,30 @@
+﻿Public Class frmVctoresNumericos
+    Dim Mes() As String = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"}
+    Dim Ventas(11) As Decimal
+
+    Private Sub btnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+        Dim i As Integer = cmbMes.SelectedIndex
+        Ventas(i) = Ventas(i) + txtImporte.Text
+        MessageBox.Show("Datos cargados correctamente")
+        txtImporte.Text = ""
+    End Sub
+
+    Private Sub frmVctoresNumericos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Integer = 0
+        While i < 12
+            cmbMes.Items.Add(Mes(i))
+            i = i + 1
+        End While
+        cmbMes.SelectedIndex = 0
+    End Sub
+
+    Private Sub btnListar_Click(sender As Object, e As EventArgs) Handles btnListar.Click
+        Dim i As Integer = 0
+        dgvVentas.Rows.Clear()
+
+        While i < 12
+            dgvVentas.Rows.Add(Mes(i), Ventas(i))
+            i = i + 1
+        End While
+    End Sub
+End Class
